60a6419f95ac5a5383204a2ad6e2afb6f313eda9
Noor Elshafei <noorelshafei@paymob.com>
2024-05-18 23:01:35 +0300

    first commit

---
+++ app/src/main/java/com/halan/twittertask/Application.kt
@@ -0,0 +1,22 @@
+package com.halan.twittertask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        installTimber()
+    }
+
+
+
+    private fun installTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
